a5d852ac0d1980f90e330396d85fab1b3ff1299b
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-11-06 12:54:49 +0100

    Refine ListPaymentMethod component

--- src/components/paymentMethods/ListPaymentMethod.vue
+++ src/components/paymentMethods/ListPaymentMethod.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <h2>PaymentMethods List</h2>
-    <button @click="fetchPaymentMethods" class="btn btn-primary mb-3">Add PaymentMethods</button>
+    <h2>Listed des paymentMethods</h2>
+    <button @click="addPaymentMethod" class="btn btn-primary mb-3">Ajouter un paymentMethod</button>
     <table class="table table-striped">
       <thead>
         <tr>
           <th>Name</th>
           <th>Status</th>
-          <th>User</th>
+          <th>Utilisateur</th>
           <th>Actions</th>
         </tr>
       </thead>
@@ -17,9 +17,10 @@
           <td>{{ method.status ? 'Active' : 'Inactive' }}</td>
           <td>{{ method.user?.username || 'Not assigned' }}</td>
           <td>
-            <button @click="editMethod(method.id)" class="btn btn-warning btn-sm">Edit</button>
-            <button @click="deleteMethod(method.id)" class="btn btn-danger btn-sm">Delete</button>
-          </td>
+            <button @click="editmethod(method.id)" class="btn btn-warning btn-sm me-2"><i class="fas fa-edit"></i> </button>
+            <button @click="deletemethod(method.id)" class="btn btn-danger btn-sm me-2"><i class="fas fa-trash-alt"></i> </button>
+            <button @click="viewmethod(method.id)" class="btn btn-info btn-sm me-2"><i class="fas fa-eye"></i> </button> 
+           </td>
         </tr>
       </tbody>
     </table>
@@ -28,20 +29,39 @@
 
 <script setup>
 import { computed, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import { usePaymentMethodStore } from '@/stores/PaymentMethodStore.js';
 
 const paymentMethodStore = usePaymentMethodStore();
+const router = useRouter();
 
 const fetchPaymentMethods = async () => {
   await paymentMethodStore.loadPaymentMethods();
 };
 
-const editMethod = (methodId) => {
-  console.log("Édition de la méthode avec l'ID:", methodId);
+const addPaymentMethod = () => {
+  router.push({ name: 'addPaymentMethod' });
 };
 
-const deleteMethod = async (methodId) => {
-  await paymentMethodStore.deletePaymentMethod(methodId); 
+const editmethod = (methodId) => {
+  router.push({ name: 'editPaymentMethod', params: { id: methodId } });
+};
+
+const viewmethod = (methodId) => {
+  router.push({ name: 'viewPaymentMethod', params: { id: methodId } });
+};
+
+const deletemethod = async (methodId) => {
+  const confirmDelete = window.confirm("Êtes-vous sûr de vouloir supprimer cette méthode de paiement ?");
+  
+  if (confirmDelete) {
+    try {
+      await paymentMethodStore.removePaymentMethod(methodId);
+      await fetchPaymentMethods();
+    } catch (error) {
+      console.error("Erreur lors de la suppression de la méthode de paiement :", error);
+    }
+  }
 };
 
 onMounted(fetchPaymentMethods);
